fd7f53ad4734a44e5e7fbd4e347e3626ac717415
sajdeen-dev <sayyedsajdeen.dev@gmail.com>
2025-12-20 23:34:57 +0530

    fun py

---
+++ first.py
@@ -0,0 +1,17 @@
+# import keyword
+
+# print(keyword.kwlist)
+
+
+# var1 = True
+
+# print(type(var1))
+
+# var2 = 100
+# var3 = 100.1
+
+# print(var2+var3)
+
+
+name = input("Enter your name: ") 
+print("Hello, " + name)
