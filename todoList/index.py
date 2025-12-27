7ab0ad5f199206363707a4b07e70bb46c720c1f6
sajdeen-dev <sayyedsajdeen.dev@gmail.com>
2025-12-27 23:21:52 +0530

    Add index.py: implement basic task management menu with options to add, view, and delete tasks

---
+++ todoList/index.py
@@ -0,0 +1,30 @@
+tasks = []
+
+def show_menu():
+    print("1. Add a task")
+    print("2. View all tasks")
+    print("3. Delte a task ")
+    print("4. Exit")
+
+
+    def add_task():
+        pass
+        def delete_task():
+            pass
+            def view_tasks():
+                pass
+
+while True:
+    show_menu()
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_task()
+        elif choice == "2":
+            view_tasks()
+        elif choice == "3":
+            delete_task()
+        elif choice == "4":
+            break
+        else:
+            print("Invalid choice")
