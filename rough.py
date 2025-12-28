485da963c8e2660c8653283eb12fe70181af2dd3
sajdeen-dev <sayyedsajdeen.dev@gmail.com>
2025-12-28 23:18:08 +0530

    loop

--- rough.py
+++ rough.py
@@ -1,9 +1,25 @@
-l1 = [1,2,3,4,5,6,7,8,9,10]
-num_list = ['Sayyesh','Rohit','Rahul','Rajesh','Rajesh', ['a','b','c','d','e']]
+# # l1 = [1,2,3,4,5,6,7,8,9,10]
+# # num_list = ['Sayyesh','Rohit','Rahul','Rajesh','Rajesh', ['a','b','c','d','e']]
 
-l1.append(num_list)
-print(l1[-1][-1][-1])
+# # l1.append(num_list)
+# # print(l1[-1][-1][-1])
 
 
-st = {1,234,444,444,4,4,4,44,4,4,}
-print(st)
+# # st = {1,234,444,444,4,4,4,44,4,4,}
+# # print(st)
+
+
+# temp = int(input("Enter the temperature: "))
+
+# if temp>=30:
+#     print("It's warm")
+# elif temp<=20:
+#         print("It's cool")
+# else:
+#         print("It's cold")
+
+
+fruits = ['apple','banana','cherry','date','elderberry']
+
+for i in fruits:
+    print(i)
